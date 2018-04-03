0fc3b7437e410ec45cbd5d5360cdc9d6cc2d83c9
Gerard Murphy <mgerard@vmware.com>
2018-04-03 13:10:28 -0400

    HW-81533 add updates

--- Pod/SRTabItem.swift
+++ Pod/SRTabItem.swift
@@ -10,77 +10,97 @@ import Cocoa
 
 public class SRTabItem: NSButton {
 
-    /// The delegate for the item
-    weak var delegate: SRTabItemDelegate?
-    
-    /// The index of the item on the bar
-    var index = 0
-    
-    /// The view controller associated with this item
-    var viewController: NSViewController?
-    
-    // MARK: - Initializers
-    
-    init(index: Int, viewController: NSViewController) {
-        super.init(frame: NSZeroRect)
-        
-        self.index = index
-        self.viewController = viewController
-        wantsLayer = true
+	/// The delegate for the item
+	weak var delegate: SRTabItemDelegate?
+
+	/// The index of the item on the bar
+	var index = 0
+
+	var onImage : NSImage? = nil
+	var offImage : NSImage? = nil
+
+	/// The view controller associated with this item
+	public var viewController: NSViewController?
+
+	// MARK: - Initializers
+
+	init(index: Int, viewController: NSViewController) {
+		super.init(frame: NSZeroRect)
+
+		self.index = index
+		self.viewController = viewController
+		wantsLayer = true
 		isBordered = false
 		imagePosition = .imageAbove
-        setButtonType(.momentaryChange)
-        
-        if let title = viewController.title {
-            attributedTitle = NSAttributedString(string: title, attributes: [
-				NSAttributedStringKey.font: NSFont.systemFont(ofSize: 10),
-				NSAttributedStringKey.foregroundColor: NSColor.white
-            ])
-        } else {
-            title = ""
+		setButtonType(.momentaryChange)
+
+		if let title = viewController.title {
+			attributedTitle = NSAttributedString(string: title, attributes: [
+				NSFontAttributeName: NSFont.systemFont(ofSize: 10),
+				NSForegroundColorAttributeName: NSColor.blue
+				])
+		} else {
+			title = ""
 			imagePosition = .imageOnly
-        }
-        
-        (cell as? NSButtonCell)?.highlightsBy = .changeBackgroundCellMask
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public override func viewDidMoveToSuperview() {
-        super.viewDidMoveToSuperview()
-        
-        target = self
-        action = #selector(buttonPressed)
-    }
-    
-    // MARK: - Actions
-    
+		}
+
+		(cell as? NSButtonCell)?.highlightsBy = .changeBackgroundCellMask
+	}
+
+	required public init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	public override func viewDidMoveToSuperview() {
+		super.viewDidMoveToSuperview()
+
+		target = self
+		action = #selector(buttonPressed)
+	}
+
+	// MARK: - Actions
+
 	@objc func buttonPressed() {
 		delegate?.tabIndexShouldChangeTo(index: index)
-    }
-    
-    func setTintColor(tint: NSColor) {
-        
-        attributedTitle = NSAttributedString(string: title, attributes: [
-			NSAttributedStringKey.font: NSFont.systemFont(ofSize: 10),
-			NSAttributedStringKey.foregroundColor: tint
-        ])
-        
-        guard let image = image else {
-            Swift.print("Item has no image")
-            return
-        }
-        
-        image.lockFocus()
-        tint.set()
-        let imageRect = NSRect(origin: NSZeroPoint, size: image.size)
-		__NSRectFillUsingOperation(imageRect, .sourceAtop)
-        image.unlockFocus()
-        
-        self.image = image
-        
-    }
-    
+	}
+
+	func setTintColor(tint: NSColor) {
+
+		attributedTitle = NSAttributedString(string: title, attributes: [
+			NSFontAttributeName: NSFont.systemFont(ofSize: 10),
+			NSForegroundColorAttributeName: tint
+			])
+
+		guard let image = image else {
+			Swift.print("Item has no image")
+			return
+		}
+
+		image.lockFocus()
+		tint.set()
+		let imageRect = NSRect(origin: NSZeroPoint, size: image.size)
+		NSRectFillUsingOperation(imageRect, .sourceAtop)
+		image.unlockFocus()
+
+		self.image = image
+	}
+
+
+	func buttonOn() {
+		self.image = onImage
+	}
+
+	func buttonOff() {
+		self.image = offImage
+	}
+
+	override open func mouseEntered(with event: NSEvent) {
+		alphaValue = 0.5
+	}
+
+	override open func mouseExited(with event: NSEvent) {
+		alphaValue = 1
+	}
+
+
 }
